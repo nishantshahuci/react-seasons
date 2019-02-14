9c196a8d4e31b937eae94bb9be64da8067356e82
Nishant Shah <nishantshahuci@gmail.com>
2019-02-14 13:58:37 -0500

    Add App constructor and initialize state

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import ReactDOM from 'react-dom';
 // App component
 class App extends Component {
 
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            lat: null
+        };
+    }
+
+    // must define this function
     render () {
 
         // get the user's current position
