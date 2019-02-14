3c39da18045e55bd3358813638b785566b805243
Nishant Shah <nishantshahuci@gmail.com>
2019-02-14 13:29:24 -0500

    Refactor App to use a class and get the user location

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,10 @@
+// import react
+import React from 'react';
+
+// SeasonDisplay component
+const SeasonDisplay = () => {
+    return <div>Season Display</div>;
+}
+
+// export component
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 // import react
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 // App component
-const App = () => {
-    return <div>Hello World!</div>;
-}
+class App extends Component {
+
+    render () {
+
+        // get the user's current position
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                console.log(position);
+            },
+            error => {
+                console.log(error);
+            }
+        );
+
+        return <div>Latitude: </div>;
+    }
+
+};
 
 // render App
 ReactDOM.render(<App />, document.querySelector('#root'));
